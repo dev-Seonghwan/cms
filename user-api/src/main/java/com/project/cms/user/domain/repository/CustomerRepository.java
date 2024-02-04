fd981bb223c154b8a5b88a2a3023363ffd26be17
seonghwan <norte0190@gmail.com>
2024-02-05 06:26:46 +0900

    feat : [003-EMAIL-VERIFICATION] 이메일 VERIFICATION

--- user-api/src/main/java/com/project/cms/user/domain/repository/CustomerRepository.java
+++ user-api/src/main/java/com/project/cms/user/domain/repository/CustomerRepository.java
@@ -1,10 +1,13 @@
 package com.project.cms.user.domain.repository;
 
 import com.project.cms.user.domain.model.Customer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional <Customer> findByEmail(String email);
+
 
 }
